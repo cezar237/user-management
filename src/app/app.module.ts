13673f2aed1a7ffada241b34c440ffe9e7c55f74
Daniel Adelrich Tadjuifouo <DTadjuifouo@groupecephas.com>
2023-04-27 22:48:54 +0100

    le user-list crée

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserContainerComponent } from './components/user-container/user-container.component';
+import { UserListComponent } from './components/user-container/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserContainerComponent
+    UserContainerComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
